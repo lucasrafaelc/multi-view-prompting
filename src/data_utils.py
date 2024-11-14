bd684e772e046eafb1c203cd490b1a7b35a4f966
Lucas Rafael Costella Pessutto <lucasrafaelc@gmail.com>
2024-11-14 14:06:49 -0300

    a

--- src/data_utils.py
+++ src/data_utils.py
@@ -38,7 +38,6 @@ def get_orders(task, data, args, sents, labels):
     #                                         args.task)
 
     if args.single_view_type == 'rank':
-        print(optim_orders_all)
         orders = optim_orders_all[task][data]
     elif args.single_view_type == 'rand':
         orders = [random.Random(args.seed).choice(
